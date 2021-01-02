2bf6624ff0ca0c56b2a0caa3d07ab12e23cd2cf1
brinkqiang <brink.qiang@gmail.com>
2021-01-02 21:28:17 +0800

    add

--- thirdparty/dmgen4pborm/thirdparty/dmtemplate/cmake/ModuleImport.cmake
+++ thirdparty/dmgen4pborm/thirdparty/dmtemplate/cmake/ModuleImport.cmake
@@ -266,24 +266,52 @@ macro(ModuleImportAll ModulePath)
     ENDIF()
 endmacro(ModuleImportAll)
 
-macro(ModuleConfigure ModuleName)
+macro(ModuleConfigure ModuleName ModulePath)
     IF (WIN32)
         ADD_CUSTOM_TARGET(
             ${ModuleName}_configure
             COMMAND echo "${ModuleName}_config"
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            WORKING_DIRECTORY ${ModulePath}
             )
     ELSEIF (APPLE)
         ADD_CUSTOM_TARGET(
             ${ModuleName}_configure
             COMMAND glibtoolize && aclocal && autoheader && autoconf && automake --add-missing && sh configure
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            WORKING_DIRECTORY ${ModulePath}
             )
     ELSEIF (UNIX)
         ADD_CUSTOM_TARGET(
             ${ModuleName}_configure
             COMMAND libtoolize && aclocal && autoheader && autoconf && automake --add-missing && sh configure
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            WORKING_DIRECTORY ${ModulePath}
             )
     ENDIF()
+
+    ADD_DEPENDENCIES(${ModuleName} ${ModuleName}_configure)
 endmacro(ModuleConfigure)
+
+macro(ModuleCommand ModuleName ModulePath CommandLine)
+    MESSAGE(STATUS "ModuleCommand ${ModuleName} ${ModulePath} ${CommandLine}")
+
+    IF (WIN32)
+        ADD_CUSTOM_TARGET(
+            ${ModuleName}_command
+            COMMAND ${CommandLine}
+            WORKING_DIRECTORY ${ModulePath}
+            )
+    ELSEIF (APPLE)
+        ADD_CUSTOM_TARGET(
+            ${ModuleName}_command
+            COMMAND ${CommandLine}
+            WORKING_DIRECTORY ${ModulePath}
+            )
+    ELSEIF (UNIX)
+        ADD_CUSTOM_TARGET(
+            ${ModuleName}_command
+            COMMAND ${CommandLine}
+            WORKING_DIRECTORY ${ModulePath}
+            )
+    ENDIF()
+
+    ADD_DEPENDENCIES(${ModuleName} ${ModuleName}_command)
+endmacro(ModuleCommand)
