ec0c8753a955141ecfa6c2c29967c76b8495f197
brinkqiang <brink.qiang@gmail.com>
2021-01-04 14:56:59 +0800

    add

--- thirdparty/dmlibgo/thirdparty/select_asm.sh
+++ thirdparty/dmlibgo/thirdparty/select_asm.sh
@@ -9,5 +9,5 @@ obj=`./b2 -an --with-context link=static | grep "compile.asm" | awk '{print $2}'
 obj_name=`basename $obj`
 source=`echo $obj_name | cut -d\. -f1`.S
 file=$work/thirdparty/boost.context/libs/context/src/asm/$source
-cp $file $work/libgo/context
-echo -n $work/libgo/context/$source
+cp $file $work/src/libgo/context
+echo -n $work/src/libgo/context/$source
