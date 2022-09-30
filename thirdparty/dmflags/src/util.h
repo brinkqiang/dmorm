ca1757f9048c6ff312125db3571cfe6bdc983d5b
brinkqiang <brinkqiang@gmail.com>
2022-09-30 16:22:10 +0800

    add

--- thirdparty/dmflags/src/util.h
+++ thirdparty/dmflags/src/util.h
@@ -261,10 +261,10 @@ inline void MakeTmpdir(std::string* path) {
 #else
 inline void MakeTmpdir(std::string* path) {
   if (!path->empty()) {
-	int err = mkdir(path->c_str(), 0755);
+	int err = mkdir(path->c_str(), S_IRWXU | S_IRWXG | S_IXOTH);
 	if (err == 0 || errno == EEXIST) return;
   }
-  mkdir("/tmp/gflags_unittest", 0755);
+  mkdir("/tmp/gflags_unittest", S_IRWXU | S_IRWXG | S_IXOTH);
 }
 #endif
 
