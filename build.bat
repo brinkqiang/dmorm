2bb0144476ccd4095cba3d10442d50288a51c146
brinkqiang <brinkqiang@gmail.com>
2022-09-30 14:05:35 +0800

    add

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ rmdir /S /Q build
 mkdir build
 pushd build
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cmake --build . --config relwithdebinfo
+cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
 popd
 
 rem pause
